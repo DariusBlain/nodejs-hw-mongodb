b1b70aefc17e2602a670f89ed9d2cf334f033377
Serhii Syrov <surov.sergey.m@gmail.com>
2024-10-24 17:53:28 +0200

    status code

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -24,7 +24,7 @@ export const getContactsController = async (req, res) => {
     userId,
   });
 
-  res.json({
+  res.status(200).json({
     status: 200,
     message: 'Successfully found contacts!',
     data: contacts,
@@ -40,7 +40,7 @@ export const getContactByIdController = async (req, res) => {
     throw createHttpError(404, 'Contact not found');
   }
 
-  res.json({
+  res.status(200).json({
     status: 200,
     message: `Successfully found contact with id ${contactId}!`,
     data: contact,
@@ -85,7 +85,7 @@ export const patchContactController = async (req, res, next) => {
     return;
   }
 
-  res.json({
+  res.status(200).json({
     status: 200,
     message: 'Successfully patched a contact!',
     data: result.contact,
